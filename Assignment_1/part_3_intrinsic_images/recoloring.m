0a69bd0cf6591fa97392f8960e1f850679ed68d6
john <ioannis.tsiamas@gmail.com>
2019-02-13 13:49:22 +0100

    part 3 done

--- Assignment_1/part_3_intrinsic_images/recoloring.m
+++ Assignment_1/part_3_intrinsic_images/recoloring.m
@@ -2,15 +2,38 @@ original_im = imread("ball.png");
 albedo_im = imread("ball_albedo.png");
 shading_im = imread("ball_shading.png");
 
-puregreen_im = original_im;
-puregreen_im(:,:,[1,3]) = 0;
-
 figure(1)
+color_distribution(albedo_im)
+% title("Color Distribution - Uniform")
 
-subplot(1, 2, 1)
+greenalbedo_im = albedo_im;
+greenalbedo_im(:, :, [1 3]) = 0;
+puregreen_im = uint8(uint16(greenalbedo_im) .* uint16(shading_im) ./ 255);
+
+figure(2)
+subplot(1, 3, 1)
 imshow(original_im)
 title("original")
-
-subplot(1, 2, 2)
+subplot(1, 3, 2)
+imshow(greenalbedo_im)
+title("green albedo")
+subplot(1, 3, 3)
 imshow(puregreen_im)
-title("pure green")
+title("green recolored")
+
+figure(3)
+color_distribution(puregreen_im)
+title("Color Distribution - pure green")
+
+function [] = color_distribution(im)
+R = im(:,:,1);
+G = im(:,:,2);
+B = im(:,:,3);
+
+[yR, ~] = imhist(R);
+[yG, ~] = imhist(G);
+[yB, x] = imhist(B);
+
+plot(x, yR, 'Red', x, yG, 'Green', x, yB, 'Blue');
+
+end
