893bfb0df8d75e99e4f493c6311e448d3856d0cb
Unknown <Kassapiselias@hotmail.co.uk>
2019-02-23 19:40:32 +0100

    Denoise and PSNR functions done

--- Assignment_2/Part-1_Image Enhancement/denoise.m
+++ Assignment_2/Part-1_Image Enhancement/denoise.m
@@ -2,10 +2,14 @@ function [ imOut ] = denoise( image, kernel_type, varargin)
 
 switch kernel_type
     case 'box'
-        fprintf('Not implemented\n')
+        kernel_size = varargin{:};
+        imOut = imboxfilt(image,kernel_size);
     case 'median'
-        fprintf('Not implemented\n')
+        kernel_size = varargin{:};
+        imOut = medfilt2(image,[kernel_size kernel_size]);
     case 'gaussian'
-        fprintf('Not implemented\n')
+        kernel_size = varargin{1};
+        sigma = varargin{2};
+        imOut = imfilter(image, gauss2D(sigma , kernel_size));
 end
 end
