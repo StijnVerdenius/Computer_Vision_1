ee112d89358ff30b16c987e09a2f7871921b9567
Unknown <Kassapiselias@hotmail.co.uk>
2019-02-27 20:31:18 +0100

    finalised parts for q6 and q7

--- Assignment_2/Part-1_Image_Enhancement/Question_6.m
+++ Assignment_2/Part-1_Image_Enhancement/Question_6.m
@@ -4,7 +4,26 @@
 disp('The PSNR between image1 saltpepper.jpg and image1.jpg is:') 
 myPSNR('images/image1_saltpepper.jpg', 'images/image1.jpg')
 
+figure(1)
+
+subplot(1,2,1)
+imshow(imread('images/image1_saltpepper.jpg'))
+title('Saltpepper image')
+
+subplot(1,2,2)
+imshow(imread('images/image1.jpg'))
+title('Original image')
+
 %Part 2
 disp('the PSNR between image1 gaussian.jpg and image1.jpg is:') 
 myPSNR('images/image1_gaussian.jpg', 'images/image1.jpg')
 
+figure(2)
+
+subplot(1,2,1)
+imshow(imread('images/image1_gaussian.jpg'))
+title('Gaussian image')
+
+subplot(1,2,2)
+imshow(imread('images/image1.jpg'))
+title('Original image')
