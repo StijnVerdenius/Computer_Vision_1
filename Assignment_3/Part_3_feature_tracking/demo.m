e1ed150ee148cc7f0d2afb267049f04f212978b5
Stijn Verdenius <stijnverdenius@pop-os.localdomain>
2019-03-05 17:21:25 +0100

    still setting up ass 3 part 3

--- Assignment_3/Part_3_feature_tracking/demo.m
+++ Assignment_3/Part_3_feature_tracking/demo.m
@@ -6,10 +6,11 @@ path2 = "./person_toy/"; % if run from assignment_3 folder
 % path2 = "./../person_toy/"; % if run from part_3 folder
 
 pingpong_imgs = read_directory(path1, '*', ".jpeg");
-toy_imgs = read_directory(path2, '*', ".jpg");
+% toy_imgs = read_directory(path2, '*', ".jpg");
+
 
 tracked_pingpong = tracking(pingpong_imgs);
-tracked_toy = tracking(toy_imgs);
+% tracked_toy = tracking(toy_imgs);
 
 % show(tracked_pingpong)
 % show(tracked_toy)
