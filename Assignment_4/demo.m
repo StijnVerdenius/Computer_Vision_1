68abf4f7c7250ca09cc213de4c95a749fca9a147
Unknown <Kassapiselias@hotmail.co.uk>
2019-03-11 16:08:28 +0100

    Added Q1.1-2 to demo

--- Assignment_4/demo.m
+++ Assignment_4/demo.m
@@ -1,4 +1,13 @@
 
+%Question 1
+
+%Part 1
+[m,s] = keypoint_matching('boat1.pgm', 'boat2.pgm');
+
+%Part 2
+get_random_matchpoints('boat1.pgm', 'boat2.pgm');
+
+
 % Compute parameters of img1 to img2 to transform
 [m1,m2,m3,m4,t1,t2] = RANSAC('boat2.pgm','boat1.pgm');
 current_params = [m1,m2,m3,m4,t1,t2];
