636c7ee0e3b789145c9746f1a27f6f4de585d456
john <ioannis.tsiamas@gmail.com>
2019-03-15 17:35:17 +0100

    sift_descriptor_extraction works for all seven channels

--- Final_Assignment/Part_1/preprocess/Feature_Extraction/sift_descriptor_extraction.m
+++ Final_Assignment/Part_1/preprocess/Feature_Extraction/sift_descriptor_extraction.m
@@ -1,19 +1,31 @@
-function descriptors = sift_descriptor_extraction(img, method)
+function descriptors_concat = sift_descriptor_extraction(img, method)
 % transforms image to sift descriptor (gray-scale only) (NOT FINISHED)
 
+descriptors = cell(7,1);
+
 % TODO: include these somehow, not very clear from assignment
-gray_img = ConvertColorSpace(img, 'gray'); % transform to grayscale
-% [~] = ConvertColorSpace(img, 'opponent');
+img_gray = ConvertColorSpace(img, 'gray'); % transform to grayscale
+img_opp = ConvertColorSpace(img, 'opponent');
 
 % TODO: presmoothing???
 
 if (strcmp( method, "dense"))
-    [~, descriptors] = vl_dsift(im2single(gray_img), 'step', 3, 'size', 9, 'fast'); % get descriptors
+    [~, descriptors{1}] = vl_dsift(im2single(img_gray), 'step', 3, 'size', 9, 'fast');
+    for channel=1:3
+        [~, descriptors{1 + channel}] = vl_dsift(im2single(img_opp(:, :, channel)), 'step', 3, 'size', 9, 'fast');
+        [~, descriptors{4 + channel}] = vl_dsift(im2single(img(:, :, channel)), 'step', 3, 'size', 9, 'fast');
+    end
 elseif (strcmp( method, "keypoint"))
-    [~, descriptors] = vl_sift(im2single(gray_img)); % get descriptors
+    [~, descriptors{1}] = vl_sift(im2single(img_gray)); % get descriptors
+    for channel=1:3
+        [~, descriptors{1 + channel}] = vl_sift(im2single(img_opp(:, :, channel)));
+        [~, descriptors{4 + channel}] = vl_sift(im2single(img(:, :, channel)));
+    end
 else
     error("unrecognized sift option: "+ method);
 
+descriptors_concat = cell2mat(descriptors);
+    
 % TODO: tune vldsift parameters
 % TODO: support rgb-sift and opponent-sift.
 % TODO: check if the right descriptors are being returned.
