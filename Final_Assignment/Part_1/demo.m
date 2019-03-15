1c2ee4c84425edce026d266ac8fd5a51a15fe812
Stijn Verdenius <stijnverdenius@pop-os.localdomain>
2019-03-15 17:59:38 +0100

    optimized k-means

--- Final_Assignment/Part_1/demo.m
+++ Final_Assignment/Part_1/demo.m
@@ -7,15 +7,15 @@ vl_setup()
 
 %% extract data for vocabulary
 
-[vocab_building_imgs, ~] = load_image_data("train" , 0.5, false, 1, ["all"], true); % get all classes from trainset to generate vocabulary
+[vocab_building_imgs, ~] = load_image_data("train" , 0.05, true, 1, ["all"], true); % get all classes from trainset to generate vocabulary
 
 
 %% create vocabulary
 
-random_number = 50;                        % According to assignment either 400, 1000 or 4000
-loading_if_possible = true;                % defines whether vocabulary will be loaded from cache
+vocabulary_size = 4000;                    % According to assignment either 400, 1000 or 4000
+loading_vocab_if_possible = false;          % defines whether vocabulary will be loaded from cache
 saving_when_done = true;                   % defines whether it will be cached after generating, given its not loaded
-vocab = create_vocabulary(vocab_building_imgs, random_number, loading_if_possible, saving_when_done); % creates vocabulary
+vocab = create_vocabulary(vocab_building_imgs, vocabulary_size, loading_vocab_if_possible, saving_when_done); % creates vocabulary
 
 
 %% extract data for training and testing
@@ -23,10 +23,10 @@ vocab = create_vocabulary(vocab_building_imgs, random_number, loading_if_possibl
 wanted_classes = ["airplane", "bird", "ship", "horse", "car"];
 
 % change the following for different data selection
-start_index = 1; % index from which to start loading data in cas of non random loading
-percentage_of_data = 0.05; % percentage of data loaded into model
-random_selection = false; % wether selection is random images
-two_dimensional_pictures = true; % load pictures into vectors or plottable images
+start_index = 1;                         % index from which to start loading data in cas of non random loading
+percentage_of_data = 0.05;               % percentage of data loaded into model
+random_selection = false;                % wether selection is random images
+two_dimensional_pictures = true;         % load pictures into vectors or plottable images
 
 [train_im, train_label] = load_image_data("train" , percentage_of_data, random_selection, start_index, wanted_classes, two_dimensional_pictures);
 [test_im, test_label] = load_image_data("test" , percentage_of_data, random_selection, start_index, wanted_classes, two_dimensional_pictures);
@@ -34,11 +34,11 @@ two_dimensional_pictures = true; % load pictures into vectors or plottable image
 
 %% convert to bag of words
 
-loading_if_possible = true; % same as before
-saving_when_done = true; % same as before
-sift_method = "keypoint"; % dense or keypoint
-BoW_train_imgs = bagging_images(train_im, vocab, loading_if_possible, saving_when_done, "train", sift_method); % transform images to BoW representation
-BoW_test_imgs = bagging_images(test_im, vocab, loading_if_possible, saving_when_done, "test", sift_method); % transform images to BoW representation
+loading_bow_if_possible = false;                           % same as before
+saving_when_done = true;                                  % same as before
+sift_method = "dense";                                 % = dense or keypoint
+BoW_train_imgs = bagging_images(train_im, vocab, loading_bow_if_possible, saving_when_done, "train", sift_method); % transform images to BoW representation
+BoW_test_imgs = bagging_images(test_im, vocab, loading_bow_if_possible, saving_when_done, "test", sift_method); % transform images to BoW representation
 
 %% train models
 
--- Final_Assignment/Part_1/preprocess/kmeans/find_clusters.m
+++ Final_Assignment/Part_1/preprocess/kmeans/find_clusters.m
@@ -3,11 +3,21 @@ function cluster_centers = find_clusters(descriptors, number_of_clusters)
 
 disp("started finding clusters");
 
-% get rid of order
+% get rid of order (bag of words paradigm)
 unordered = reshape(descriptors, 128, size(descriptors, 2) * size(descriptors, 3));
 
 % cluster descriptors, get cluster centroids
-[cluster_centers] = vl_kmeans(unordered, number_of_clusters);
+if (number_of_clusters < 1000)
+    [cluster_centers] = vl_kmeans(unordered, number_of_clusters);
+    
+    
+% elseif(number_of_clusters<1500)    % TODO: test elkan, does it really work better for a 1000??
+%     [cluster_centers] = vl_kmeans(unordered, number_of_clusters,'algorithm', 'ELKAN');
+
+
+else
+    [cluster_centers] = vl_kmeans(unordered, number_of_clusters,'algorithm', 'ANN');
+end
 
 disp("finished finding clusters");
 
