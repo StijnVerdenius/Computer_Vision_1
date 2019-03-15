ad724a9693746586b1dac902d3f6936e5da0c0b1
Unknown <Kassapiselias@hotmail.co.uk>
2019-03-15 09:21:36 +0100

    added liblinear, and comment in the train_model and test_model functions

--- Final_Assignment/Part_1/test_model.m
+++ Final_Assignment/Part_1/test_model.m
@@ -1,5 +1,9 @@
-function acc = test_model(model, imgs, labels) % EX. 2.6
+function acc = test_model(model, imgs, labels) % EX. 2.6 --- is this the prefictor part?
 % trains a SVM model with bows representations. (NOT FINISHED)
+
+% is this the prefictor part? If yes this is the function used below    
+% ScoreSVMModel = fitPosterior(SVMModel,X,Y); %this fits a score-to-posterior-probability transformation function to the scores
+%[label,score] = predict(SVMModel,newX);
 
 % TODO: entire function
 acc =0;
--- Final_Assignment/Part_1/train_model.m
+++ Final_Assignment/Part_1/train_model.m
@@ -5,4 +5,41 @@ function models = train_model(bows, labels) % EX. 2.5
 % TODO: download and learn LIBLINEAR-SVM (see assignment)
 
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% THIS IS IF WE USE LIBLINEAR
+
+% % need to run 'make' in matlab console first (Directory = liblinear-2.21/matlab) 
+% %----> May need to change the matlab directory in MATLABDIR in Makefile for this!!!!!
+
+% make; 
+
+% model = train(labels, bows[,'liblinear_options', 'col']); 
+
+%This is the command given in the liblinear package
+
+%         -labels:
+%             An m by 1 vector of training labels. (type must be double)
+%         -bows:
+%             An m by n matrix of m training instances with n features.
+%             It must be a sparse matrix. (type must be double)
+%         -liblinear_options:
+%             A string of training options in the same format as that of LIBLINEAR.
+%         -col:
+%             if 'col' is set, each column of training_instance_matrix is a data instance. Otherwise each row is a data instance.
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% SVM alternative below
+
+% 
+% model = fitcsvm(bows,labels,'KernelFunction','rbf','Standardize',true,'ClassNames',{'negClass','posClass'});
+% 
+% % bows — Matrix of predictor data, where each row is one observation, and each column is one predictor.
+% 
+% % labels — Array of class labels with each row corresponding to the value of the corresponding row in Bows.
+% 
+% % KernelFunction — The default value is 'linear' for two-class learning, which separates the data by a hyperplane. The value 'gaussian' (or 'rbf') is the default for one-class learning, and specifies to use the Gaussian (or radial basis function) kernel. 
+% 
+% % Standardize — Flag indicating whether we should standardize the predictors before training the classifier.
+% 
+% % ClassNames — Distinguishes between the negative and positive classes, or specifies which classes to include in the data. The negative class is the first element (or row of a character array), e.g., 'negClass', and the positive class is the second element (or row of a character array), e.g., 'posClass'. ClassNames must be the same data type as Y. 
+
+ model = fitcsvm(bows, labels);
 end
