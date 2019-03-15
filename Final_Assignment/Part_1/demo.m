59fe90ebb9a17aa4e1dc7b386bf97b7e043e55f9
Filip Knyszewski <filip_mateus96@hotmail.com>
2019-03-15 16:07:21 +0100

    bow done

--- Final_Assignment/Part_1/demo.m
+++ Final_Assignment/Part_1/demo.m
@@ -5,7 +5,6 @@ close all
 clear all
 vl_setup()
 
-
 %% extract data for vocabulary
 
 [vocab_building_imgs, ~] = load_image_data("train" , 0.05, false, 1, ["all"], true);
@@ -35,7 +34,7 @@ two_dimensional_pictures = true;
 
 %% convert to bag of words
 
-loading_if_possible = true;
+loading_if_possible = false;
 saving_when_done = true;
 BoW_train_imgs = bagging_images(train_im, vocab, loading_if_possible, saving_when_done, "train");
 BoW_test_imgs = bagging_images(test_im, vocab, loading_if_possible, saving_when_done, "test");
--- Final_Assignment/Part_1/preprocess/bow/bag_of_words.m
+++ Final_Assignment/Part_1/preprocess/bow/bag_of_words.m
@@ -7,9 +7,27 @@ vocab_size = size(vocab, 2);
 descriptors = sift_descriptor_extraction(img); 
 bow = zeros(vocab_size, 1);
 
+
+for d = 1:size(descriptors,2)
+    descriptor = double(descriptors(:,d));
+    best_cluster = -1;
+    best_distance = intmax;
+    for i= 1:vocab_size
+        cluster_center = vocab(:,i);
+        distance = norm(descriptor - cluster_center);
+        if distance < best_distance
+            best_distance = distance;
+            best_cluster = i;
+        end
+    end
+    bow(best_cluster,1) = bow(best_cluster) + 1;
+end
+
 % TODO: match descriptors against vocab and then count (EX. 2.3). Subsequently,
 % return bag of words frequencies for image (EX. 2.4)
 
-% things to look at for matching: vl_feat buitlin function, other inspirations: https://stackoverflow.com/questions/23104750/bag-of-words-bow-in-vlfeat
+% things to look at for matching: vl_feat buitlin function, 
+% other inspirations: https://stackoverflow.com/questions/23104750/bag-of-words-bow-in-vlfeat
 
+% output should be |V|x1
 end
